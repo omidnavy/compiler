d100ae794d98a526bd12b0f8cf68737ac1014ba9
Omid Navy <omidnavy@yahoo.com>
2019-12-09 17:08:55 +0330

    syntactic analyzer completed

--- components/parser/syntactic/index.js
+++ components/parser/syntactic/index.js
@@ -14,33 +14,23 @@ const parenthesize = tokens => {
         return expression;
     }
 
-    return token;
+    else return token;
 };
 
 const parse = tokens => {
     if (Array.isArray(tokens)) {
         const [first, ...rest] = tokens;
-        return {
-            type: 'CallExpression',
-            name: first.value,
-            arguments: rest.map(parse),
-        };
+        return { type: 'CallExpression', name: first.value, arguments: rest.map(parse) };
     }
-
     else {
         const { type, value } = tokens;
-
         if (type === 'Number')
             return { type: 'NumericLiteral', value };
-
         if (token.type === 'String')
             return { type: 'StringLiteral', value };
-
         if (token.type === 'Name')
             return { type: 'Identifier', name: value };
-
     }
-
 };
 
 module.exports = { parse: tokens => parse(parenthesize(tokens)) };
